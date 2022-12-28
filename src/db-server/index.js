1026f4fbaec9df1810f6807ac1297adc733fb47e
Anton Trafimovich <antont@compatibl.com>
2022-12-29 00:59:43 +0100

    feat: use delete record request handler in db server.

--- src/db-server/index.js
+++ src/db-server/index.js
@@ -1,21 +1,23 @@
 import http from "node:http";
-import process from "node:process";
 
 import { CreateRecordRequestHandler } from "./handlers/create-record-request-handler.js";
 import { CreateSegmentRequestHandler } from "./handlers/create-segment-request-handler.js";
+import { DeleteRecordRequestHandler } from "./handlers/delete-record-request-handler.js";
 
 const server = http.createServer();
 
 const addRequestHandlers = (server) => {
-  const createRecordRequestHandler = new CreateRecordRequestHandler();
+  const deleteRecordRequestHandler = new DeleteRecordRequestHandler();
+  const createRecordRequestHandler = new CreateRecordRequestHandler(
+    deleteRecordRequestHandler
+  );
   const requestHandlers = new CreateSegmentRequestHandler(
     createRecordRequestHandler
   );
 
-  return server.on(
-    "request",
-    async (req, res) => await requestHandlers.handle(req, res)
-  );
+  return server.on("request", async (req, res) => {
+    await requestHandlers.handle(req, res);
+  });
 };
 
 addRequestHandlers(server).listen(1337, () => {
