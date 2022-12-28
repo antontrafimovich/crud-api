1663b02c4089fb00daecb41969e50b6be627b09c
Anton Trafimovich <antont@compatibl.com>
2022-12-28 12:42:06 +0100

    fix: fix imports in server's handlers.

--- src/server/handlers/index.js
+++ src/server/handlers/index.js
@@ -1,3 +1,3 @@
-export * from "./create-user-requests-handler.js";
+export * from "./create-user-request-handler.js";
 export * from "./get-users-request-handler.js";
 export * from "./not-existing-requests-handler.js";
