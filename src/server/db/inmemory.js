d410974d1153269e063bdb23893ff34ebb1a2b72
Anton Trafimovich <antont@compatibl.com>
2022-12-29 23:38:59 +0100

    fix: change inmemory db methods' implementation.

--- src/server/db/inmemory.js
+++ src/server/db/inmemory.js
@@ -4,11 +4,20 @@ class InmemoryDbSegment {
   _storage = [];
 
   getAll() {
-    return Promise.resolve([...this._storage]);
+    return Promise.resolve({ records: [...this._storage] });
   }
 
   getById(id) {
-    return Promise.resolve(this._storage.find((item) => item.id === id));
+    const item = this._storage.find((item) => item.id === id);
+
+    if (!item) {
+      return Promise.reject({
+        statusCode: 404,
+        message: `There's no record with an id ${id}`,
+      });
+    }
+
+    return Promise.resolve({ record: item });
   }
 
   create(record) {
@@ -19,19 +28,39 @@ class InmemoryDbSegment {
 
     this._storage.push(newRecord);
 
-    return Promise.resolve(newRecord);
+    return Promise.resolve({ record: newRecord });
   }
 
-  update(record) {
+  update(id, record) {
+    const item = this._storage.find((item) => item.id === id);
+
+    if (!item) {
+      return Promise.reject({
+        statusCode: 404,
+        message: `There's no record with an id ${id}`,
+      });
+    }
+
+    const newRecord = { ...item, ...record, id: item.id };
     this._storage = this._storage.map((item) =>
-      item.id === record.id ? record : item
+      item.id === id ? newRecord : item
     );
 
-    return Promise.resolve(record);
+    return Promise.resolve({ record: newRecord });
   }
 
   delete(id) {
-    this._storage = this._storage.filter((item) => item.id === id);
+    const item = this._storage.find((item) => item.id === id);
+
+    if (!item) {
+      return Promise.reject({
+        statusCode: 404,
+        message: `There's no record with an id ${id}`,
+      });
+    }
+
+    this._storage = this._storage.filter((item) => item.id !== id);
+
     return Promise.resolve();
   }
 }
