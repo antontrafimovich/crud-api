bccab361900eca7ec48c58e054c783b58d423b70
Anton Trafimovich <antont@compatibl.com>
2023-01-07 19:56:11 +0100

    fix: change basic server files structure.

---
+++ src/server/server.ts
@@ -0,0 +1,44 @@
+import * as dotenv from "dotenv";
+import { createServer, Server } from "node:http";
+import process from "node:process";
+
+import {
+  CreateUserRequestHanlder,
+  DeleteUserRequestHanlder,
+  GetUserByIdRequestHanlder,
+  GetUsersRequestHanlder,
+  NotExistingRequestsHandler,
+  UpdateUserRequestHanlder,
+} from "./handlers";
+
+dotenv.config();
+
+const server = createServer();
+
+const addRequestHandlers = (server: Server) => {
+  const requestHandlers = [
+    new NotExistingRequestsHandler(),
+    new DeleteUserRequestHanlder(),
+    new CreateUserRequestHanlder(),
+    new UpdateUserRequestHanlder(),
+    new GetUsersRequestHanlder(),
+    new GetUserByIdRequestHanlder(),
+  ].reduce((result, handler) => {
+    return handler.setNext(result);
+  }, undefined);
+
+  return server.on(
+    "request",
+    async (req, res) => await requestHandlers.handle(req, res)
+  );
+};
+
+const startServer = (): Server => {
+  return addRequestHandlers(server)
+    .listen(process.env.AT_CRUD_API_PORT, () => {
+      console.log(`Server's listening ${process.env.AT_CRUD_API_PORT} port`);
+    })
+    .on("error", console.log);
+};
+
+export { startServer };
