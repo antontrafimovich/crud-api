85fb5e52d5ca920d6f616948ed4494ab408cbaf7
Anton Trafimovich <antont@compatibl.com>
2023-01-10 11:49:16 +0100

    feat: add 2 more test scenarios.

--- __test__/integration.test.ts
+++ __test__/integration.test.ts
@@ -153,3 +153,259 @@ describe("GET POST GET_BY_ID PUT DELETE GET_BY_ID Scenario", () => {
     request.end();
   });
 });
+
+describe("POST DELETE GET PUT Scenario", () => {
+  let createdUser: User & { id: string };
+
+  test("Create user", (done) => {
+    const request = http.request("http://localhost:8000/api/users", {
+      method: "POST",
+    });
+
+    const user = {
+      name: "Anton",
+      age: 26,
+      hobbies: ["guitar"],
+    };
+
+    request.write(JSON.stringify(user));
+
+    request.on("response", async (res) => {
+      const respsonse = await streamToPromise(res);
+      const { data } = JSON.parse(respsonse);
+
+      createdUser = data;
+      const { id, ...resultUser } = data;
+
+      expect(res.statusCode).toBe(200);
+      expect(user).toStrictEqual(resultUser);
+      expect(id).toBeDefined();
+      done();
+    });
+
+    request.end();
+  });
+
+  test("Delete user", (done) => {
+    const request = http.request(
+      `http://localhost:8000/api/users/${createdUser.id}`,
+      {
+        method: "DELETE",
+      }
+    );
+
+    request.on("response", async (res) => {
+      const response = await streamToPromise(res);
+      console.log(response);
+
+      expect(res.statusCode).toBe(204);
+      done();
+    });
+
+    request.end();
+  });
+
+  test("Get deleted user by id", (done) => {
+    const request = http.request(
+      `http://localhost:8000/api/users/${createdUser.id}`,
+      {
+        method: "GET",
+      }
+    );
+
+    request.on("response", async (res) => {
+      const response = await streamToPromise(res);
+
+      expect(res.statusCode).toBe(404);
+      expect(response).toBe(`User with id ${createdUser.id} doesn't exist`);
+      done();
+    });
+
+    request.end();
+  });
+
+  test("Update deleted user by id", (done) => {
+    const request = http.request(
+      `http://localhost:8000/api/users/${createdUser.id}`,
+      {
+        method: "PUT",
+      }
+    );
+
+    const updatedUser = {
+      ...createdUser,
+      age: 27,
+      hobbies: ["football", "guitar"],
+    };
+
+    request.write(JSON.stringify(updatedUser));
+
+    request.on("response", async (res) => {
+      const response = await streamToPromise(res);
+
+      expect(res.statusCode).toBe(404);
+      expect(response).toBe(`User with id ${createdUser.id} doesn't exist`);
+      done();
+    });
+
+    request.end();
+  });
+});
+
+describe("POST POST GET UPDATE GET_BY_ID DELETE GET Scenario", () => {
+  let createdUser: User & { id: string };
+  let updatedUser: User & { id: string };
+
+  test("Create first user", (done) => {
+    const request = http.request("http://localhost:8000/api/users", {
+      method: "POST",
+    });
+
+    const user = {
+      name: "Anton",
+      age: 26,
+      hobbies: ["guitar"],
+    };
+
+    request.write(JSON.stringify(user));
+
+    request.on("response", async (res) => {
+      const respsonse = await streamToPromise(res);
+      const { data } = JSON.parse(respsonse);
+
+      createdUser = data;
+      const { id, ...resultUser } = data;
+
+      expect(res.statusCode).toBe(200);
+      expect(user).toStrictEqual(resultUser);
+      expect(id).toBeDefined();
+      done();
+    });
+
+    request.end();
+  });
+
+  test("Create second user without required param", (done) => {
+    const request = http.request("http://localhost:8000/api/users", {
+      method: "POST",
+    });
+
+    const user = {
+      name: "John",
+      age: 30,
+    };
+
+    request.write(JSON.stringify(user));
+
+    request.on("response", async (res) => {
+      const response = await streamToPromise(res);
+
+      expect(res.statusCode).toBe(400);
+      expect(response).toBe(`Value for hobbies param(s) is mandatory`);
+      done();
+    });
+
+    request.end();
+  });
+
+  test("Get all users", (done) => {
+    const request = http.request("http://localhost:8000/api/users", {
+      method: "GET",
+    });
+
+    request.on("response", async (res) => {
+      const response = await streamToPromise(res);
+      const { data } = JSON.parse(response);
+
+      expect(res.statusCode).toBe(200);
+      expect(data.length).toBe(1);
+      done();
+    });
+
+    request.end();
+  });
+
+  test("Update user by id", (done) => {
+    const request = http.request(
+      `http://localhost:8000/api/users/${createdUser.id}`,
+      {
+        method: "PUT",
+      }
+    );
+
+    updatedUser = {
+      ...createdUser,
+      age: 32,
+      hobbies: ["football", "guitar"],
+    };
+
+    request.write(JSON.stringify(updatedUser));
+
+    request.on("response", async (res) => {
+      const respsonse = await streamToPromise(res);
+      const { data } = JSON.parse(respsonse);
+
+      expect(res.statusCode).toBe(200);
+      expect(data).toStrictEqual(updatedUser);
+      done();
+    });
+
+    request.end();
+  });
+
+  test("Get updated user by id", (done) => {
+    const request = http.request(
+      `http://localhost:8000/api/users/${createdUser.id}`,
+      {
+        method: "GET",
+      }
+    );
+
+    request.on("response", async (res) => {
+      const response = await streamToPromise(res);
+      const { data } = JSON.parse(response);
+
+      expect(res.statusCode).toBe(200);
+      expect(data).toStrictEqual(updatedUser);
+      done();
+    });
+
+    request.end();
+  });
+
+  test("Delete user", (done) => {
+    const request = http.request(
+      `http://localhost:8000/api/users/${createdUser.id}`,
+      {
+        method: "DELETE",
+      }
+    );
+
+    request.on("response", async (res) => {
+      const response = await streamToPromise(res);
+      console.log(response);
+
+      expect(res.statusCode).toBe(204);
+      done();
+    });
+
+    request.end();
+  });
+
+  test("Get all users", (done) => {
+    const request = http.request("http://localhost:8000/api/users", {
+      method: "GET",
+    });
+
+    request.on("response", async (res) => {
+      const response = await streamToPromise(res);
+      const { data } = JSON.parse(response);
+
+      expect(res.statusCode).toBe(200);
+      expect(data.length).toBe(0);
+      done();
+    });
+
+    request.end();
+  });
+});
